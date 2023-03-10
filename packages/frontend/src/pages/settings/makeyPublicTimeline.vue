0d246e23ef62e4d0ba34bfec0d7042c517765885
Takenori Morio <morikapusan@moritoworks.com>
2023-03-11 02:12:27 +0900

    リロードの説明を分かりやすくなるように修正

--- packages/frontend/src/pages/settings/makeyPublicTimeline.vue
+++ packages/frontend/src/pages/settings/makeyPublicTimeline.vue
@@ -2,7 +2,7 @@
 <div class="_gaps_m">
 	<MkSwitch v-model="makeyHideLocalTimeline" @update:model-value="save()">ローカル・ソーシャルを非表示にする<template #caption>この設定をオンにしてもノートの公開範囲によっては公開タイムラインに掲載されます。この設定を画面に反映するにはリロードする必要があります。</template></MkSwitch>
 	<MkSwitch v-model="makeyHideFederatedTimeline" @update:model-value="save()">グローバルを非表示にする<template #caption>この設定をオンにしてもノートの公開範囲によっては公開タイムラインに掲載されます。この設定を画面に反映するにはリロードする必要があります。</template></MkSwitch>
-	<MkButton primary @click="reloadPage"><i class="ti ti-refresh"></i>リロード</MkButton>
+	<MkButton primary @click="reloadPage"><i class="ti ti-refresh"></i>リロードして反映する</MkButton>
 </div>
 </template>
 
