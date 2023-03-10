913523428fb09df7bb8fe9e5eb7563b31e970254
Takenori Morio <morikapusan@moritoworks.com>
2023-03-11 01:57:52 +0900

    リロードボタン追加

--- packages/frontend/src/pages/settings/makeyPublicTimeline.vue
+++ packages/frontend/src/pages/settings/makeyPublicTimeline.vue
@@ -2,18 +2,15 @@
 <div class="_gaps_m">
 	<MkSwitch v-model="makeyHideLocalTimeline" @update:model-value="save()">ローカル・ソーシャルを非表示にする<template #caption>この設定をオンにしてもノートの公開範囲によっては公開タイムラインに掲載されます。この設定を画面に反映するにはリロードする必要があります。</template></MkSwitch>
 	<MkSwitch v-model="makeyHideFederatedTimeline" @update:model-value="save()">グローバルを非表示にする<template #caption>この設定をオンにしてもノートの公開範囲によっては公開タイムラインに掲載されます。この設定を画面に反映するにはリロードする必要があります。</template></MkSwitch>
+	<MkButton primary @click="reloadPage"><i class="ti ti-refresh"></i>リロード</MkButton>
 </div>
 </template>
 
 <script lang="ts" setup>
 import { } from 'vue';
 import MkSwitch from '@/components/MkSwitch.vue';
-import MkSelect from '@/components/MkSelect.vue';
-import FormSection from '@/components/form/section.vue';
-import MkFolder from '@/components/MkFolder.vue';
+import MkButton from '@/components/MkButton.vue';
 import * as os from '@/os';
-import { defaultStore } from '@/store';
-import { i18n } from '@/i18n';
 import { $i } from '@/account';
 import { definePageMetadata } from '@/scripts/page-metadata';
 
@@ -26,6 +23,10 @@ function save() {
 		makeyHideLocalTimeline: !!makeyHideLocalTimeline,
 		makeyHideFederatedTimeline: !!makeyHideFederatedTimeline,
 	});
+}
+
+async function reloadPage() {
+	location.reload();
 }
 
 const headerActions = $computed(() => []);
