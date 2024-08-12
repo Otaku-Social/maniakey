975df1b34d7564ae71d3669444ba966bbc863450
tmorio <20278135+tmorio@users.noreply.github.com>
2024-08-12 23:56:48 +0900

    公開タイムライン非表示機能の設定を適用されているポリシーを元に切り替え可能に

--- packages/frontend/src/pages/settings/makeyPublicTimeline.vue
+++ packages/frontend/src/pages/settings/makeyPublicTimeline.vue
@@ -1,8 +1,12 @@
 <template>
 <div class="_gaps_m">
 	<MkInfo>これらの設定をオンにしてもノートの公開範囲によっては公開タイムラインに掲載されます。画面に反映するにはリロードしてください。</MkInfo>
-	<MkSwitch v-model="makeyHideLocalTimeline" @update:model-value="save()">ローカル・ソーシャルを非表示にする</MkSwitch>
-	<MkSwitch v-model="makeyHideFederatedTimeline" @update:model-value="save()">グローバルを非表示にする</MkSwitch>
+	<div v-if="instance.policies.ltlAvailable && $i.policies.ltlAvailable">
+		<MkSwitch v-model="makeyHideLocalTimeline" @update:model-value="save()">ローカル・ソーシャルを非表示にする</MkSwitch>
+	</div>
+	<div v-if="instance.policies.gtlAvailable && $i.policies.gtlAvailable">
+		<MkSwitch v-model="makeyHideFederatedTimeline" @update:model-value="save()">グローバルを非表示にする</MkSwitch>
+	</div>
 	<MkButton primary @click="reloadPage"><i class="ti ti-refresh"></i>リロードして反映する</MkButton>
 </div>
 </template>
@@ -13,6 +17,7 @@ import MkSwitch from '@/components/MkSwitch.vue';
 import MkButton from '@/components/MkButton.vue';
 import MkInfo from '@/components/MkInfo.vue';
 import { $i } from '@/account.js';
+import { instance } from '@/instance.js';
 import { definePageMetadata } from '@/scripts/page-metadata.js';
 import { misskeyApi } from "@/scripts/misskey-api.js";
 
